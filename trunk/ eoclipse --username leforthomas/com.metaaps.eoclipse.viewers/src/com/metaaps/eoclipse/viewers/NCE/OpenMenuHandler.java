8e564510fe7490eb67cf5378fdd5e9dc03147d23
leforthomas@gmail.com <leforthomas@gmail.com@a0368982-22c3-9538-c0db-3257e5a39b0c>
2010-09-09 14:21:43 +0000

    Added copyrights

--- trunk/ eoclipse --username leforthomas/com.metaaps.eoclipse.viewers/src/com/metaaps/eoclipse/viewers/NCE/OpenMenuHandler.java
+++ trunk/ eoclipse --username leforthomas/com.metaaps.eoclipse.viewers/src/com/metaaps/eoclipse/viewers/NCE/OpenMenuHandler.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 METAAPS SRL(U).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     METAAPS SRL(U) - created by Thomas Lefort - initial API and implementation
+ ******************************************************************************/
 package com.metaaps.eoclipse.viewers.NCE;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
