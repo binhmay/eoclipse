88724fe3fbd682c2d3edd8e370e395a94d59dc5f
leforthomas@gmail.com <leforthomas@gmail.com@a0368982-22c3-9538-c0db-3257e5a39b0c>
2010-10-06 11:57:38 +0000

    Initial import.
    Original Code From Francois Xavier Thoorens
    Adapted for EOClipse by Thomas Lefort - Metaaps SRL(U)

---
+++ trunk/ eoclipse --username leforthomas/com.metaaps.eoclipse.vectordatareader/src/com/metaaps/eoclipse/vectordatareader/ShapeFileReader.java
@@ -0,0 +1,198 @@
+/*******************************************************************************
+ * Copyright (c) 2010 METAAPS SRL(U).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     METAAPS SRL(U) - created by Thomas Lefort - initial API and implementation
+ ******************************************************************************/
+package com.metaaps.eoclipse.vectordatareader;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.DataUtilities;
+import org.geotools.data.DefaultQuery;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.FileDataStoreFactorySpi;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.data.shapefile.indexed.IndexedShapefileDataStoreFactory;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.factory.GeoTools;
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.geotools.feature.FeatureType;
+import org.geotools.feature.SchemaException;
+import org.geotools.feature.SimpleFeatureType;
+import org.geotools.filter.text.cql2.CQL;
+import org.geotools.filter.text.cql2.CQLException;
+import org.opengis.feature.Feature;
+import org.opengis.feature.type.PropertyDescriptor;
+import org.opengis.filter.Filter;
+import org.opengis.filter.FilterFactory;
+
+import com.metaaps.eoclipse.common.Attributes;
+import com.metaaps.eoclipse.common.CodeFragment;
+import com.metaaps.eoclipse.common.datasets.DataContent;
+import com.metaaps.eoclipse.common.datasets.ISourceDataContent;
+import com.metaaps.eoclipse.common.datasets.IVectorData;
+import com.metaaps.eoclipse.common.datasets.VectorData;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.io.WKTReader;
+
+public class ShapeFileReader extends DataContent implements IVectorData, ISourceDataContent {
+
+	private String m_name;
+	private File m_file;
+	private DataStore m_dataStore;
+
+	@Override
+	public String getName() {
+		return (m_file == null ? null : m_file.getName());
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setFile(File file) {
+		m_file = file;
+	}
+
+	@Override
+	public boolean initialise() {
+		if(m_file != null) {
+	        try {
+	        	Map<String, Serializable> connectParameters = new HashMap<String, Serializable>();
+	        	connectParameters.put("url", m_file.toURI().toURL());
+	            m_dataStore = new ShapefileDataStore(m_file.toURI().toURL());
+//	            m_dataStore = DataStoreFinder.getDataStore(connectParameters);
+	            if(m_dataStore != null) {
+	            	return true;
+	            }
+            
+	        } catch (Exception ex) {
+				ex.printStackTrace();
+	        }
+		}
+		
+		return false;
+	}
+
+	@Override
+	public void save(File file) {
+	}
+
+    private static String[] createSchema(Collection<PropertyDescriptor> attributeTypes) {
+        String[] out = new String[attributeTypes.size()];
+        int i = 0;
+        for (PropertyDescriptor at : attributeTypes) {
+            out[i++] = at.getName().toString();
+        }
+        return out;
+    }
+
+    private static String[] createTypes(Collection<PropertyDescriptor> attributeTypes) {
+        String[] out = new String[attributeTypes.size()];
+        int i = 0;
+        for (PropertyDescriptor at : attributeTypes) {
+            out[i++] = at.getType().getBinding().getSimpleName();
+        }
+        return out;
+    }
+
+	@Override
+	public VectorData getVectorData(String cqlFilter) {
+		VectorData vectordata = new VectorData();
+		
+		if(m_dataStore != null) {
+	        // needs feature name
+			try {
+				FeatureSource featureSource = m_dataStore.getFeatureSource(getGeometryName());
+		        String geomName = featureSource.getSchema().getGeometryDescriptor().getLocalName();
+	            FeatureCollection featurecollection = null;
+		        if(cqlFilter.length() != 0) {
+		            Filter filter = CQL.toFilter(cqlFilter);
+		            DefaultQuery query = new DefaultQuery(featureSource.getSchema().getName().getLocalPart(), filter);
+		            featurecollection = featureSource.getFeatures(query);
+		        } else {
+		            featurecollection = featureSource.getFeatures();
+		        }
+
+		        if (!featurecollection.isEmpty()) {
+			        FeatureIterator fi = featurecollection.features();
+		            String[] schema = createSchema(featurecollection.getSchema().getDescriptors());
+		            String[] types = createTypes(featurecollection.getSchema().getDescriptors());
+			        while (fi.hasNext()) {
+			            Feature f = fi.next();
+			            try {
+			                Attributes at = Attributes.createAttributes(schema, types);
+			                for (int i = 0; i < f.getProperties().size(); i++) {
+			                    at.set(schema[i], f.getProperty(schema[i]).getValue());
+			                }
+			                vectordata.put((Geometry) f.getDefaultGeometryProperty().getValue(), at);
+	
+			            } catch (Exception e) {
+							e.printStackTrace();
+			            }
+			        }
+		        }
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (CQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+		}
+		
+		return vectordata;
+	}
+
+	@Override
+	public HashMap<String, Object> getProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGeometryName() {
+		String featurename = "";
+		if(m_dataStore != null) {
+			try {
+				featurename = m_dataStore.getTypeNames()[0];
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return featurename;
+	}
+
+	@Override
+	public String[] getFilesList() {
+		return new String[]{m_file.getAbsolutePath()};
+	}
+
+}
