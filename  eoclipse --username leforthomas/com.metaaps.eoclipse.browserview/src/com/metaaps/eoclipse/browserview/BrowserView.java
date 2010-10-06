758004cbdb30aee9ead497d2ef73bc3ae4d061e6
leforthomas <leforthomas@gmail.com>
2010-10-06 11:12:19 +0000

    Initial import.
    Created by Thomas Lefort - Metaaps SRL(U)

---
+++  eoclipse --username leforthomas/com.metaaps.eoclipse.browserview/src/com/metaaps/eoclipse/browserview/BrowserView.java
@@ -0,0 +1,153 @@
+/*******************************************************************************
+ * Copyright (c) 2010 METAAPS SRL(U).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     METAAPS SRL(U) - created by Thomas Lefort - initial API and implementation
+ ******************************************************************************/
+package com.metaaps.eoclipse.browserview;
+
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.part.*;
+import org.eclipse.jface.viewers.*;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.jface.action.*;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.*;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTError;
+
+import com.metaaps.eoclipse.common.IModelChangeListener;
+import com.metaaps.eoclipse.common.IWorkFlow;
+import com.metaaps.eoclipse.common.datasets.IDataSets;
+import com.metaaps.eoclipse.common.views.IViewerImplementation;
+
+
+/**
+ * This sample class demonstrates how to plug-in a new
+ * workbench view. The view shows data obtained from the
+ * model. The sample creates a dummy model on the fly,
+ * but a real implementation would connect to the model
+ * available either in this or another plug-in (e.g. the workspace).
+ * The view is connected to the model using a content provider.
+ * <p>
+ * The view uses a label provider to define how model
+ * objects should be presented in the view. Each
+ * view can present the same model objects using
+ * different labels and icons, if needed. Alternatively,
+ * a single label provider can be shared between views
+ * in order to ensure that objects of the same type are
+ * presented in the same way everywhere.
+ * <p>
+ */
+
+public class BrowserView extends ViewPart implements IViewerImplementation, IModelChangeListener {
+
+	private Action action1;
+
+	private IWorkFlow m_workflow;
+
+	/**
+	 * The constructor.
+	 */
+	public BrowserView() {
+	}
+
+	/**
+	 * This is a callback that will allow us
+	 * to create the viewer and initialize it.
+	 */
+	public void createPartControl(Composite parent) {
+	      Browser browser;
+	      try {
+	         browser = new Browser(parent, SWT.NONE);
+		  } catch (SWTError e) {
+	         MessageBox messageBox = new MessageBox((Shell) parent, SWT.ICON_ERROR | SWT.OK);
+	         messageBox.setMessage("Browser cannot be initialized.");
+	         messageBox.setText("Exit");
+	         messageBox.open();
+	         System.exit(-1);
+	      }
+
+		makeActions();
+		contributeToActionBars();
+	}
+
+	private void contributeToActionBars() {
+		IActionBars bars = getViewSite().getActionBars();
+		fillLocalPullDown(bars.getMenuManager());
+		fillLocalToolBar(bars.getToolBarManager());
+	}
+
+	private void fillLocalPullDown(IMenuManager manager) {
+		manager.add(action1);
+	}
+
+	private void fillContextMenu(IMenuManager manager) {
+		manager.add(action1);
+		// Other plug-ins can contribute there actions here
+		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+	}
+	
+	private void fillLocalToolBar(IToolBarManager manager) {
+		manager.add(action1);
+	}
+
+	private void makeActions() {
+		action1 = new Action() {
+			public void run() {
+				showMessage("Action 1 executed");
+			}
+		};
+		action1.setText("Action 1");
+		action1.setToolTipText("Action 1 tooltip");
+		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
+			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+		
+	}
+
+	private void showMessage(String message) {
+		MessageDialog.openInformation(
+			null,
+			"Browser View",
+			message);
+	}
+
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
+	public void setFocus() {
+	}
+
+	public void setWorkflow(IWorkFlow workflow) {
+		m_workflow = workflow;
+		m_workflow.addListener(this);
+	}
+
+	@Override
+	public void modelChanged(Object element, String event) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void setDataSets(IDataSets datasets) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectionChanged(SelectionChangedEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
