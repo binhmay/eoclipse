19af2ccd082980ee4345a9634d84dc1b721e403c
leforthomas@gmail.com <leforthomas@gmail.com@a0368982-22c3-9538-c0db-3257e5a39b0c>
2010-10-06 11:23:17 +0000

    Initial import.
    Created by Thomas Lefort - Metaaps SRL(U)

---
+++ trunk/ eoclipse --username leforthomas/com.metaaps.eoclipse.facility/src/com/metaaps/eoclipse/facility/Facility.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2010 METAAPS SRL(U).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     METAAPS SRL(U) - created by Thomas Lefort - initial API and implementation
+ ******************************************************************************/
+package com.metaaps.eoclipse.facility;
+
+import com.metaaps.eoclipse.common.IWorkFlow;
+import com.metaaps.eoclipse.common.hosted.IFacility;
+
+public class Facility implements IFacility {
+
+	public Facility() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void host(IWorkFlow workflow) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
