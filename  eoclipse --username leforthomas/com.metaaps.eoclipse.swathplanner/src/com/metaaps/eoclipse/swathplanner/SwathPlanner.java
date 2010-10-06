8ee24fb1355b2158a90a10ad0207bad28efa6302
leforthomas <leforthomas@gmail.com>
2010-10-06 11:56:18 +0000

    Initial Import.
    All code from Thomas Lefort, Metaaps SRL(U)

---
+++  eoclipse --username leforthomas/com.metaaps.eoclipse.swathplanner/src/com/metaaps/eoclipse/swathplanner/SwathPlanner.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2010 METAAPS SRL(U).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     METAAPS SRL(U) - created by Thomas Lefort - initial API and implementation
+ ******************************************************************************/
+package com.metaaps.eoclipse.swathplanner;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class SwathPlanner extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		
+		// create a composite structure composed of a gis view control and a metadata view control
+		
+		
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
