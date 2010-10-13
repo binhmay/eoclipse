8d704cc41fc0f8ca8cd2b46294545351cb7e5106
leforthomas <leforthomas@gmail.com>
2010-10-13 15:55:49 +0000

    Added Image Masking Changes to implement the new Process Parameters

---
+++  eoclipse --username leforthomas/com.metaaps.eoclipse.genericprocessing/src/com/metaaps/eoclipse/genericprocessing/util/RasterizeVector.java
@@ -0,0 +1,58 @@
+package com.metaaps.eoclipse.genericprocessing.util;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+import com.metaaps.eoclipse.common.datasets.IVectorData;
+import com.metaaps.eoclipse.common.datasets.VectorData;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Polygon;
+
+public class RasterizeVector {
+
+    private VectorData m_vectordata;
+    
+    public RasterizeVector(VectorData vectordata) {
+    	m_vectordata = vectordata;
+	}
+
+	public RasterizeVector(IVectorData mask) {
+		// TODO Auto-generated constructor stub
+	}
+
+	// rasterize the mask clipped with the Rectangle scaled back to full size with an offset onto a BufferedImage
+    public BufferedImage rasterize(Rectangle rect, int offsetX, int offsetY, double scalingFactor) {
+        // create the buffered image of the size of the Rectangle
+        BufferedImage image = new BufferedImage(rect.width, rect.height, BufferedImage.TYPE_BYTE_BINARY);
+        GeometryFactory gf = new GeometryFactory();
+        // define the clipping region in full scale
+        Coordinate[] coords = new Coordinate[]{
+            new Coordinate((int) (((double) rect.getMinX() / scalingFactor)), (int) (((double) rect.getMinY() / scalingFactor))),
+            new Coordinate((int) (((double) rect.getMaxX() / scalingFactor)), (int) (((double) rect.getMinY() / scalingFactor))),
+            new Coordinate((int) (((double) rect.getMaxX() / scalingFactor)), (int) (((double) rect.getMaxY() / scalingFactor))),
+            new Coordinate((int) (((double) rect.getMinX() / scalingFactor)), (int) (((double) rect.getMaxY() / scalingFactor))),
+            new Coordinate((int) (((double) rect.getMinX() / scalingFactor)), (int) (((double) rect.getMinY() / scalingFactor))),};
+        Polygon geom = gf.createPolygon(gf.createLinearRing(coords), null);
+        Graphics g2d = image.getGraphics();
+        g2d.setColor(Color.WHITE);
+        for (Geometry p : m_vectordata.getGeometries()) {
+            if (p.intersects(geom)) {
+                int[] xPoints = new int[p.getNumPoints()];
+                int[] yPoints = new int[p.getNumPoints()];
+                int i = 0;
+                for (Coordinate c : p.getCoordinates()) {
+                    xPoints[i] = (int) ((c.x + offsetX) * scalingFactor);
+                    yPoints[i++] = (int) ((c.y + offsetY) * scalingFactor);
+                }
+                g2d.fillPolygon(xPoints, yPoints, p.getNumPoints());
+            }
+        }
+        g2d.dispose();
+        return image;
+    }
+
+}
